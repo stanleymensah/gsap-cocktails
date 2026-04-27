3a3b87e099fe43033f6aebc21fea760546b998e0
stanleymensah <stanleymensahjunior>
2026-04-27 17:02:49 +0000

    Add initial components and assets for the Mojito landing page
    - Created Hero component with title, images, and description.
    - Added Navbar component with GSAP animation for scroll effect.
    - Included SVG logo for branding.
    - Added input and output video files for media content.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 export default function App() {
   return (
-    <div className="flex-center h-[100vh] ">
-        <h1 className="text-3xl text-indigo-300">Hello, GSAP!</h1>
-    </div>
+    <>
+        <main>
+            <Navbar />
+            <Hero />
+        </main>
+    </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,39 @@
+export default function Hero() {
+  return (
+    <>
+      <section id="hero" className="noisy">
+        <h1 className="title">MOJITO</h1>
+        <img
+          src="/images/hero-left-leaf.png"
+          alt="left-leaf"
+          className="left-leaf"
+        />
+        <img
+          src="/images/hero-right-leaf.png"
+          alt="right-leaf"
+          className="right-leaf"
+        />
+
+        <div className="body">
+          <div className="content">
+            <div className="space-y-5 hidden md:block">
+              <p>Cool. Crisp. Classic</p>
+              <p className="subtitle">
+                Sip the Spirit <br /> of Summer
+              </p>
+            </div>
+
+            <div className="view-cocktails">
+              <p className="subtitle">
+                Every cocktail on our menu is a blend of premium ingredients,
+                creative flair, and timeless recipes - designed to delight your
+                senses.
+              </p>
+              <a href="#cocktails">View Cocktails</a>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
